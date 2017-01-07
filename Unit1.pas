ad68abfff1cf1899d6b019771fceed20bac64528
ilnurzif <ilnurzif@gmail.com>
2017-01-08 00:08:11 +0300

    8 commit

--- Unit1.pas
+++ Unit1.pas
@@ -5,12 +5,15 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
-  FMX.Controls.Presentation, FMX.StdCtrls;
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects;
 
 type
   TForm1 = class(TForm)
     Button1: TButton;
+    Button2: TButton;
+    Image1: TImage;
     procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -29,6 +32,11 @@ begin
 showmessage('23');
 caption:='12312';
 showmessage('______Ilnur111rgbrg143thty5');
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+showmessage('');
 end;
 
 end.
